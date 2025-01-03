0cdccde3f6a69d35aeb00c84fecd2bdbb27e902d
Benjamin Höglinger-Stelzer <nefarius@dhmx.at>
2025-01-03 21:05:29 +0100

    Update UnitTestParser.cs

--- tests/UnitTestParser.cs
+++ tests/UnitTestParser.cs
@@ -14,40 +14,23 @@ public class Tests
     [Test]
     public void Test1()
     {
-        string etwFilePath = @".\traces\VPadRuntime.etl";
+        string etwFilePath = @"C:\Users\Nefarius\Documents\WPR Files\WIN11-DEV-VM.01-03-2025.20-55-29.etl";
 
         MemoryStream ms = new();
         Utf8JsonWriter jsonWriter = new(ms);
 
-        if (!EtwUtil.ConvertToJson(jsonWriter, new[] { etwFilePath }, error =>
-                {
-                    Assert.Fail();
-                },
-                guid =>
-                {
-                    switch (guid.ToString().ToUpper())
-                    {
-                        case "021B2C3C-9DD6-4C0A-A53A-6183F1BE11A0":
-                            return File.OpenRead(
-                                @"D:\Development\git.nefarius.at\ViGEm Framework\library\VPadRuntimeETW.man");
-                        case "AFEBAD70-D5DB-4A74-BDA2-764D2A875AAF":
-                            // this should never hit since WPP doesn't use a manifest
-                            throw new NotImplementedException();
-                        default:
-                            return null;
-                    }
-                }))
+        if (!EtwUtil.ConvertToJson(jsonWriter, [etwFilePath], error =>
+            {
+                Assert.Fail();
+            }))
         {
             Assert.Fail();
         }
 
         ms.Seek(0, SeekOrigin.Begin);
 
-        StreamReader sr = new(ms);
-
-        string json = sr.ReadToEnd();
-        
-        File.WriteAllText("output.json", json);
+        using FileStream outFile = File.OpenWrite("output.json");
+        ms.CopyTo(outFile);
 
         Assert.Pass();
     }
