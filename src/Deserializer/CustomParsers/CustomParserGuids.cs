c44fade5c791701952f01e673e5bcaf2ec5aa70d
Benjamin Höglinger-Stelzer <nefarius@dhmx.at>
2025-01-03 13:29:09 +0100

    Added EventTraceGuid

--- src/Deserializer/CustomParsers/CustomParserGuids.cs
+++ src/Deserializer/CustomParsers/CustomParserGuids.cs
@@ -12,4 +12,9 @@ internal static class CustomParserGuids
     public static Guid KernelTraceControlMetaDataGuid = new("bbccf6c1-6cd1-48c4-80ff-839482e37671");
 
     public static Guid KernelStackWalkGuid = new("def2fe46-7bd6-4b80-bd94-f57fe20d0ce3");
+    
+    /// <summary>
+    ///     EventTraceGuid is used to identify a event tracing session.
+    /// </summary>
+    public static Guid EventTraceGuid = new("68fdd900-4a3e-11d1-84f4-0000f80464e3");
 }
