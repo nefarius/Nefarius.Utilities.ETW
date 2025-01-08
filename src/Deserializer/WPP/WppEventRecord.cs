666884a7238ab0707f8b7cd67e09974c4e7ba42d
Benjamin Höglinger-Stelzer <nefarius@dhmx.at>
2025-01-08 15:30:52 +0100

    Update WppEventRecord.cs

--- src/Deserializer/WPP/WppEventRecord.cs
+++ src/Deserializer/WPP/WppEventRecord.cs
@@ -1,4 +1,4 @@
-﻿using System.Runtime.CompilerServices;
+﻿using System.Reflection;
 using System.Runtime.InteropServices;
 using System.Runtime.InteropServices.ComTypes;
 
@@ -10,6 +10,27 @@ internal readonly unsafe struct WppEventRecord
 {
     private readonly EVENT_RECORD* _record;
     private readonly DecodingContext _decodingContext;
+
+    public uint Version { get; init; }
+    public Guid TraceGuid { get; init; }
+    public string GuidName { get; init; }
+    public string GuidTypeName { get; init; }
+    public uint ThreadId { get; init; }
+    public SYSTEMTIME SystemTime { get; init; }
+    public uint UserTime { get; init; }
+    public uint KernelTime { get; init; }
+    public uint SequenceNum { get; init; }
+    public uint ProcessId { get; init; }
+    public uint CpuNumber { get; init; }
+    public uint Indent { get; init; }
+    public string FlagsName { get; init; }
+    public string LevelName { get; init; }
+    public string FunctionName { get; init; }
+    public string ComponentName { get; init; }
+    public string SubComponentName { get; init; }
+    public string FormattedString { get; init; }
+    public FILETIME RawSystemTime { get; init; }
+    public Guid ProviderGuid { get; init; }
 
     /// <summary>
     ///     TDH supports a set of known properties for WPP events.
@@ -64,12 +85,14 @@ internal readonly unsafe struct WppEventRecord
                     ret = PInvoke.TdhGetWppProperty(decodingContext.Handle, eventRecord, propertyNameBuf, &size,
                         (byte*)buffer.ToPointer());
 
-
                     object? value = propertyType == typeof(string)
                         ? Marshal.PtrToStringUni(buffer)
                         : Marshal.PtrToStructure(buffer, propertyType);
-                    
-                    
+
+                    if (value is not null)
+                    {
+                        SetPropertyByName(this, propertyName, value);
+                    }
                 }
                 finally
                 {
@@ -77,6 +100,31 @@ internal readonly unsafe struct WppEventRecord
                 }
             }
         }
+    }
+
+    private static void SetPropertyByName(object obj, string propertyName, object value)
+    {
+        ArgumentNullException.ThrowIfNull(obj);
+
+        if (string.IsNullOrEmpty(propertyName))
+        {
+            throw new ArgumentNullException(nameof(propertyName));
+        }
+
+        PropertyInfo? propertyInfo =
+            obj.GetType().GetProperty(propertyName, BindingFlags.Public | BindingFlags.Instance);
+        if (propertyInfo == null)
+        {
+            throw new ArgumentException($"Property '{propertyName}' not found on type '{obj.GetType().Name}'.");
+        }
+
+        if (!propertyInfo.CanWrite)
+        {
+            throw new InvalidOperationException($"Property '{propertyName}' is read-only.");
+        }
+
+        object convertedValue = Convert.ChangeType(value, propertyInfo.PropertyType);
+        propertyInfo.SetValue(obj, convertedValue);
     }
 
     public void Decode()
