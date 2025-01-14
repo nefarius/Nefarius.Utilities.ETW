d182717da3e61a70d2bf99e6c3d08016db22af7d
Benjamin Höglinger-Stelzer <nefarius@dhmx.at>
2025-01-14 15:41:34 +0100

    Update PdbMetaData.cs

--- src/Deserializer/WPP/PdbMetaData.cs
+++ src/Deserializer/WPP/PdbMetaData.cs
@@ -21,7 +21,7 @@ public readonly struct PdbMetaData
     public required int Age { get; init; }
 
     /// <summary>
-    ///     Index prefix (path name) of the symbol to lookup on a symbol server.
+    ///     Index prefix (relative path name) of the symbol to lookup on a symbol server.
     /// </summary>
     public string IndexPrefix
     {
