c0d33311fcef25c1517c5a74513de3dbe2bf51d7
Benjamin Höglinger-Stelzer <nefarius@dhmx.at>
2025-05-08 23:37:53 +0200

    Added test files

--- tests/UnitTestParser.cs
+++ tests/UnitTestParser.cs
@@ -15,18 +15,21 @@ public class Tests
     [Test]
     public void WppTraceDecodingTest()
     {
-        string etwFilePath = @".\traces\VPadRuntime.etl";
+        string etwFilePath = @".\traces\BthPS3.etl";
 
         JsonWriterOptions options = new() { Indented = true };
 
         using MemoryStream ms = new();
         using Utf8JsonWriter jsonWriter = new(ms, options);
-        using DecodingContext decodingContext = new(new PdbFilesDecodingContextType(@"D:\Downloads\tmftest\nssvpd.pdb"));
+        using DecodingContext decodingContext = new(new PdbFilesDecodingContextType(
+            @".\symbols\BthPS3.pdb",
+            @".\symbols\BthPS3PSM.pdb"
+        ));
 
         if (!EtwUtil.ConvertToJson(jsonWriter, [etwFilePath], converterOptions =>
             {
                 // ReSharper disable once AccessToDisposedClosure
-                converterOptions.WppDecodingContext = decodingContext; 
+                converterOptions.WppDecodingContext = decodingContext;
             }))
         {
             Assert.Fail();
@@ -34,7 +37,7 @@ public class Tests
 
         ms.Seek(0, SeekOrigin.Begin);
 
-        using FileStream outFile = File.OpenWrite("output.json");
+        using FileStream outFile = File.OpenWrite("BthPS3.json");
         ms.CopyTo(outFile);
 
         Assert.Pass();
