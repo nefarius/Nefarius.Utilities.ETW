3b0a4939292183dbb9d5202667f82190950be2d7
Benjamin Höglinger-Stelzer <nefarius@dhmx.at>
2025-05-22 18:24:34 +0200

    Update FunctionParameter.cs

--- src/Deserializer/WPP/TMF/FunctionParameter.cs
+++ src/Deserializer/WPP/TMF/FunctionParameter.cs
@@ -68,11 +68,24 @@ public enum ItemType
 
 public readonly struct FunctionParameter
 {
+    /// <summary>
+    ///     The expression (variable) passed to the function parameter.
+    /// </summary>
     public required string Expression { get; init; }
 
+    /// <summary>
+    ///     The data type of the function parameter.
+    /// </summary>
     public required ItemType Type { get; init; }
 
+    /// <summary>
+    ///     The index used in the message format string to substitute this type with.
+    /// </summary>
     public required int Index { get; init; }
 
+    /// <summary>
+    ///     List item values as their string representation.
+    /// </summary>
+    /// <remarks>Only populated if <see cref="Type"/> is <see cref="ItemType.ItemListByte"/>.</remarks>
     public IReadOnlyDictionary<int, string>? ListItems { get; init; }
 }
