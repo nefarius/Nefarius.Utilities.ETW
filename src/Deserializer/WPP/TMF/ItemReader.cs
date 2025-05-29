640ce052b3468ae6eefc9cca06942e5f37df6587
Benjamin Höglinger-Stelzer <nefarius@dhmx.at>
2025-05-29 18:05:25 +0200

    Update ItemReader.cs

--- src/Deserializer/WPP/TMF/ItemReader.cs
+++ src/Deserializer/WPP/TMF/ItemReader.cs
@@ -7,6 +7,8 @@ internal static class ItemReader
     public static readonly ReadOnlyDictionary<ItemType, Func<EventRecordReader, object>> Readers =
         new Dictionary<ItemType, Func<EventRecordReader, object>>
         {
+            #region basic arithmetic types
+
             [ItemType.ItemLong] = r => r.ReadInt32(),
             [ItemType.ItemLongLong] = r => r.ReadInt64(),
             [ItemType.ItemLongLongX] = r => r.ReadInt64(),
@@ -17,10 +19,18 @@ internal static class ItemReader
             [ItemType.ItemChar] = r => r.ReadInt8(),
             [ItemType.ItemUChar] = r => r.ReadUInt8(),
             [ItemType.ItemShort] = r => r.ReadInt16(),
-            
+
+            #endregion
+
+            #region arch dependent types
+
             [ItemType.ItemPtr] = r => r.ReadPointer(),
             [ItemType.ItemGuid] = r => r.ReadGuid(),
-            
+
+            #endregion
+
+            #region Complex types
+
             [ItemType.ItemString] = r => r.ReadAnsiString(),
             [ItemType.ItemRString] = r => r.ReadAnsiString(),
             [ItemType.ItemRWString] = r => r.ReadUnicodeString(),
@@ -29,11 +39,21 @@ internal static class ItemReader
             [ItemType.ItemPWString] = r => r.ReadCountedString(),
             [ItemType.ItemSid] = r => r.ReadSid(),
             [ItemType.ItemHexDump] = r => r.ReadHexDump(),
-            
+
+            #endregion
+
+            #region enumeration types
+
             [ItemType.ItemListByte] = r => r.ReadUInt8(),
-            
+
+            #endregion
+
+            #region special formats
+
             [ItemType.ItemNTSTATUS] = r => r.ReadUInt32(),
             [ItemType.ItemWINERROR] = r => r.ReadUInt32(),
             [ItemType.ItemHRESULT] = r => r.ReadInt32()
+
+            #endregion
         }.AsReadOnly();
 }
