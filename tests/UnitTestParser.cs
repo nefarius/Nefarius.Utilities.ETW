baf4039ed24fa7456583c219f5669ba8ac2bb026
Benjamin Höglinger-Stelzer <nefarius@dhmx.at>
2025-05-29 21:09:59 +0200

    Update UnitTestParser.cs

--- tests/UnitTestParser.cs
+++ tests/UnitTestParser.cs
@@ -1,5 +1,7 @@
 using System.Collections.ObjectModel;
+using System.Text.Json;
 
+using Nefarius.Utilities.ETW.Deserializer.WPP;
 using Nefarius.Utilities.ETW.Deserializer.WPP.TMF;
 
 namespace Nefarius.Utilities.ETW.Tests;
@@ -96,6 +98,35 @@ public class Tests
     public void BthPs3EtlTraceDecodingTest()
     {
         Assert.That(Shared.BthPs3EtlTraceDecoding(), Is.True);
+    }
+
+    [Test]
+    public void BthPs3EtlTraceIncompleteContextTest()
+    {
+        const string etwFilePath = @".\traces\BthPS3_0.etl";
+
+        JsonWriterOptions options = new() { Indented = true };
+
+        using MemoryStream ms = new();
+        using Utf8JsonWriter jsonWriter = new(ms, options);
+        DecodingContext decodingContext = new(PdbFileDecodingContextType.CreateFrom(
+            @".\symbols\BthPS3.pdb"
+        ));
+
+        if (!EtwUtil.ConvertToJson(jsonWriter, [etwFilePath], converterOptions =>
+            {
+                converterOptions.WppDecodingContext = decodingContext;
+            }))
+        {
+            Assert.Fail();
+        }
+
+        ms.Seek(0, SeekOrigin.Begin);
+
+        using FileStream outFile = File.OpenWrite("BthPS3_0_partial.json");
+        ms.CopyTo(outFile);
+
+        Assert.Pass();
     }
 
     [Test]
